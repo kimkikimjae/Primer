8575e435072b3c0ca346b6d96c067c920b388f35
KijaeKim <kkw2920@naver.com>
2025-12-28 08:49:27 +0900

    Update 10.1 ~ 10.2

--- Ch10/10.1_10.2.cpp
+++ Ch10/10.1_10.2.cpp
@@ -4,5 +4,7 @@ using namespace std;
 int main()
 {
 	cout << "Hello World!" << endl;
+	cout << "But I Pray for you all the time. If I could be by your side I'll give you all my life, my seasons" << endl;
+	cout << "wave to earth";
 	return 0;
 }
