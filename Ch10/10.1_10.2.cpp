90df5596eb228ea126ff6a7bcd63ce790acb8d75
KijaeKim <kkw2920@naver.com>
2025-12-29 16:06:05 +0900

    -

--- Ch10/10.1_10.2.cpp
+++ Ch10/10.1_10.2.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <list>
+
 using namespace std;
 
-int main()
-{
-	cout << "Hello World!" << endl;
-	cout << "But I Pray for you all the time. If I could be by your side I'll give you all my life, my seasons" << endl;
-	cout << "wave to earth";
+void ex_10_1(){
+	vector<int> vec;
+	int arr[10] = { 1, 1, 3, 2, 4, 1, 3, 3, 2, 2};
+	for (const int& i : arr) vec.push_back(i);
+	auto it = count(vec.begin(), vec.end(), 1);
+	cout << it << endl;
+}
+
+void ex_10_2() {
+	list<string> lis;
+	string arr[5] = { "aaa", "bbb", "ccc", "aaa", "aaa" };
+	for (const string& s : arr) lis.push_back(s);
+	auto it = count(lis.begin(), lis.end(), "aaa");
+	cout << it << endl;
+}
+
+int main() {
+	ex_10_1();
+	ex_10_2();
 	return 0;
 }
