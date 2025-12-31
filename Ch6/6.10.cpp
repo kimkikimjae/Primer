90201bff572d067ddb2f434efe41b8215f1688ab
KijaeKim <kkw2920@naver.com>
2025-12-31 12:58:16 +0900

    6.10

---
+++ Ch6/6.10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void ex_6_10(int* pa, int* pb) {
+	int temp;
+	temp = *pa;
+	*pa = *pb;
+	*pb = temp;
+}
+
+int main() {
+	int a = 10, b = 20;
+	cout << "before change: a = " << a << " and b = " << b << '\n';
+	ex_6_10(&a, &b);
+	cout << "after change : a = " << a << " and b = " << b << endl;
+	return 0;
+}
