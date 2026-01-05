e28b4ce5198dda2d24fae336845f8f4dd992aa6e
KijaeKim <kkw2920@naver.com>
2026-01-05 23:07:46 +0900

    7.1

---
+++ Ch7/7.1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <istream>
+#include <ostream>
+using namespace std;
+
+struct Sales_data {
+	string bookNo;
+	unsigned int units_sold = 0;
+	double revenue = .0;
+};
+
+int main() {
+	Sales_data total;
+	if (cin >> total.bookNo >> total.units_sold >> total.revenue) {
+		Sales_data trans;
+		while (cin >> trans.bookNo >> trans.units_sold >> trans.revenue) {
+			if (total.bookNo == trans.bookNo) {
+				total.units_sold += trans.units_sold;
+				total.revenue += trans.revenue;
+			}
+			else {
+				cout << total.bookNo << " " << total.units_sold << " " << total.revenue << endl;
+				total.bookNo = trans.bookNo;
+				total.units_sold = trans.units_sold;
+				total.revenue = trans.revenue;
+			}
+		}
+		cout << total.bookNo << " " << total.units_sold << " " << total.revenue << endl;
+	}
+	else {
+		cerr << "No data ?!" << endl;
+		return -1;
+	}
+	return 0;
+}
