eeb452602cbebee0637eecc7e3b48b169d1afaec
KijaeKim <kkw2920@naver.com>
2026-01-07 00:56:24 +0900

    7.33

---
+++ Ch7/7.33.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Screen.h"
+
+/*	7.33.
+*	typename 'pos' precedes by Screen::size() and it is not included in class range.
+*	change 'pos' to 'Screen::pos' instead.
+*/
