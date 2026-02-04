4f2b1dd08b726f18a9c61a71e1fafda81795f886
kimkikimjae <kkw2920@naver.com>
2026-02-05 00:19:48 +0900

    update jetriders test commit

---
+++ Ch6/Ch6jettest.cpp
@@ -0,0 +1,7 @@
+﻿#include <iostream>
+int main()
+{
+    std::cout << "this file has multiple main() but so what?" << '\n';
+    std::cout << "I'm not gonna run this file, just for testing jetrider" << std::endl;
+    return 0;
+}
