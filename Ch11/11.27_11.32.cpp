187151b48092ce1bb85ef1b0918a163bafcc4d59
kimkikimjae <kkw2920@naver.com>
2026-02-05 15:31:44 +0900

    Add 11.27_11.32.cpp

---
+++ Ch11/11.27_11.32.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <iterator>
+#include <vector>
+
+// 11.24.
+void ex_11_24() {
+	// m has nothing at first.
+	std::map<int, int> m;
+
+	// when you implement [] operation, map automatically makes memory for element which has key == 0.
+	// and that element has value == 1.
+	m[0] = 1;
+}
+
+// 11.25.
+void ex_11_25() {
+	std::vector<int> v;
+	// unlike map, vector must have its own element memories before value allocation.
+	// below code is not error at grammar check, but makes a trouble at running.
+	v[0] = 1;
+}
+
