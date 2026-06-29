905a7c01df42957b25d80301037fa77b2f60fc73
kimkikimjae <kkw2920@naver.com>
2026-06-29 15:59:50 +0900

    Update Ch1 files

---
+++ Ch1/Ch1/1.1_1.2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void ex_1_1() {
+	// Source files must to be named as with .c or .cpp
+	// using Visual Studio 2022 from Microsoft
+}
+
+// for ex_1.1
+//int main()
+//{
+//	return 0;
+//}
+
+int main() {
+	// for ex_1.2
+	return -1;
+}
