43129ca45808f5c39113ed7b6bc2c1c21b490f0e
kimkikimjae <kkw2920@naver.com>
2026-07-11 01:52:16 +0900

    Delete print_total.cpp

--- Ch15/print_total.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <ostream>
-#include "Quote.h"
-
-double print_total(std::ostream& os, const Quote& item, std::size_t n) {
-	double ret = item.net_price(n);
-	os << item.isbn() << " " << n << " " << ret << std::endl;
-	return ret;
-}
