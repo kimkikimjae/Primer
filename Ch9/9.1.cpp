c70754c9bb6fd6c0b247c7217bfa58f18fef7793
kimkikimjae <kkw2920@naver.com>
2026-07-20 23:21:27 +0900

    Create 9.1.cpp

---
+++ Ch9/9.1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+void ex_9_1() {
+	// (a) maybe list will be appropriate, because according to those words that be written, they might be randomly composed in sentence
+		// and this means that a lot of operations would be happening in the middle of container, rather than being on the both ends.
+
+	// (b) deque, because operations occur only on the both end sides.
+
+	// (c) all of them could be possible, because sorting has no relationship with the kind of containers. No matter by what type of container is composed.
+}
