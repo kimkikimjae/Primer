d7b39354588b209f469213e897330bacfa60927f
kimkikimjae <kkw2920@naver.com>
2026-07-21 12:52:41 +0900

    Update 9.43_9.46

---
+++ Ch9/9.43_9.46.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+void ex_9_43(std::string& s, std::string oldVal, std::string newVal) {
+	std::string::iterator it = s.begin();
+
+	while (it != s.end()) {
+		if (std::string(it, it + oldVal.size()) == oldVal) {
+			s.erase(it, it + oldVal.size());
+			it = s.insert(it, newVal.begin(), newVal.end());
+			it += newVal.size();
+		}
+		else
+			++it;
+	}
+}
+
+int main() {
+	
+}
