261ca084fbe01889f0710e3cff5d7573efc0ea51
kimkikimjae <kkw2920@naver.com>
2026-07-22 15:50:47 +0900

    Create 9.7_9.8.cpp

---
+++ Ch9/9.7_9.8.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+
+
+void ex_9_7() {
+	// use vector<int>::size_type for index of vector.
+	std::vector<int>::size_type idx = 5;
+	std::vector<int> vec{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	std::cout << vec[idx] << '\n';
+}
+
+void ex_9_8() {
+	// use list<string>::value_type to read elements.
+	std::list<std::string> mylist{ "aaa", "bbb", "ccc", "ddd", "eee" };
+	std::list<std::string>::value_type rd = *mylist.begin();
+	std::cout << rd << '\n';
+
+	// if we write on it, we use list<string>::reference.
+	std::list<std::string>::reference wr = mylist.back();
+	wr = "fff";
+	for (const std::string& s : mylist) std::cout << s << ' ';
+}
+
+int main() {
+	ex_9_7();
+	ex_9_8();
+
+	return 0;
+}
